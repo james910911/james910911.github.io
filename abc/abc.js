01bf5ae059d402da92d226d7363d121957bb297a
james910911 <146846286+james910911@users.noreply.github.com>
2023-11-07 21:08:04 +0800

    Add files via upload

---
+++ abc/abc.js
@@ -0,0 +1,17 @@
+var x = 1 + 2 + 3;
+console.log(x);
+
+var y = "1" + 2 + 3;
+console.log(y);
+
+z = "Hello";
+console.log(z);
+
+for(i=1;i<=9;i++){
+    for(j=1;j<=9;j++){
+        k=i*j;
+        console.log(`${i}*${j}=${k}`);
+    }
+}
+
+document.getElementById("")
